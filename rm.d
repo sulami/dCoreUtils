15d84537fdc21bce78c412a4c308536d1bdf9109
Robin Schroer <sulamiification@gmail.com>
2014-08-26 15:34:45 +0200

    rm: delete directories recursively by default

--- rm.d
+++ rm.d
@@ -4,7 +4,7 @@ import std.stdio;
 int main(string[] args)
 {
     foreach (arg; args[1..args.length])
-        isDir(arg) ? rmdir(arg) : remove(arg);
+        isDir(arg) ? rmdirRecurse(arg) : remove(arg);
     return 0;
 }
 
