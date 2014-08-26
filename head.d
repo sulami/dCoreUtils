47c1aff8e88dd3c6990dbb4ca259f143f3aa49c3
Robin Schroer <sulamiification@gmail.com>
2014-08-26 10:51:58 +0200

    add first parameter to head, number of lines printed

--- head.d
+++ head.d
@@ -1,11 +1,14 @@
+import std.getopt;
 import std.stdio;
 
 int main(string[] args)
 {
     if (args.length > 1) {
+        uint lines = 10;
+        getopt(args, "n|lines", &lines);
         foreach (arg; args[1..args.length]) {
             File f = File(arg, "r");
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < lines; i++) {
                 write(f.readln());
             }
             f.close();
