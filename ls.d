f8c646c5fd27f688f5ce7c411f503551c7404c69
Robin Schroer <sulamiification@gmail.com>
2014-08-26 15:48:47 +0200

    ls: print current working directory if none specified

--- ls.d
+++ ls.d
@@ -3,9 +3,14 @@ import std.stdio;
 
 int main(string[] args)
 {
+    if (args.length == 1)
+        foreach (f; dirEntries(getcwd(), SpanMode.shallow))
+            writeln(f);
+
     foreach (arg; args[1..args.length])
         foreach (f; dirEntries(arg, SpanMode.shallow))
             writeln(f);
+
     return 0;
 }
 
