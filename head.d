2baec8645750f8f1f18a602d2c5676c045aeeace
Robin Schroer <sulamiification@gmail.com>
2014-08-26 10:56:39 +0200

    add verbose flag to head, and some formatting

--- head.d
+++ head.d
@@ -5,15 +5,20 @@ int main(string[] args)
 {
     if (args.length > 1) {
         uint lines = 10;
-        getopt(args, "n|lines", &lines);
+        bool verbose = false;
+
+        getopt(args, "n|lines", &lines, "v|verbose", &verbose);
+
         foreach (arg; args[1..args.length]) {
             File f = File(arg, "r");
-            for (int i = 0; i < lines; i++) {
+            if (verbose)
+                writeln("File: " ~ arg);
+            for (int i = 0; i < lines; i++)
                 write(f.readln());
-            }
             f.close();
         }
     }
+
     return 0;
 }
 
